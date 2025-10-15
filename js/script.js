43b9efcc282d6fe1198b1573e3dfb83aabf03528
zhtyz <zhtyz@zhtyz.zhtyz>
2025-10-15 18:02:10 -0300

    aprimorações do script.js

--- js/script.js
+++ js/script.js
@@ -2,14 +2,18 @@
 
 function submitButton(){
 
-    const email = document.getElementById('email-box').value;
-    const password = document.getElementById('password-box').value;
+    const email = document.getElementById('email-box').value.trim();
+    const password = document.getElementById('password-box').value.trim();
     let emailteste = "zezinho@gmail.com";
 
-    if(email == emailteste){
-        alert("ja existe uma conta com esse email");
-    }else{
+    if(email === "" || password === ""){
+        alert("Por favor, preencha todos os campos");
+        return;
+    }
+    if(email === emailteste){
+        alert("Já existe uma conta com esse email");
+    }
+    else{
         alert("Login permitido");
-
     }
 }
